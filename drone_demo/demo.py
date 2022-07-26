cc738c7f1776831a9323fd8948fe60eac01829cf
Salahuddin <60926009+ShisuiUzumaki@users.noreply.github.com>
2022-07-26 16:48:31 +0500

    drone_demo/demo.py updated
    drone_demo.py updated for performing inference on multiple images

--- drone_demo/demo.py
+++ drone_demo/demo.py
@@ -15,7 +15,21 @@ coco_demo = COCODemo(
     min_image_size=800,
     confidence_threshold=0.7,
 )
+
 # load image and then run prediction
+# # image = cv2.imread('visdrone_test_img_0000001_02999_d_0000005.jpg')
+# paths = glob(r'../../../input/aerialdataset/*.jpg')
+# if not os.path.exists('/kaggle/working/predictions/images'):
+#     os.makedirs('/kaggle/working/predictions/images')
+# if not os.path.exists('/kaggle/working/predictions/bbox'):
+#     os.makedirs('/kaggle/working/predictions/bbox')
+# sp_ann = '/kaggle/working/predictions/bbox'
+# for path in paths:
+#     img = cv2.imread(path)
+#     file_name = path.split('/')[-1]
+#     predictions = coco_demo.run_on_opencv_image(img, file_name, path=sp_ann)
+#     print(f'/kaggle/working/predictions/images/{file_name.split('.')[0]}_DET.jpg')
+#     cv2.imwrite(f'/kaggle/working/predictions/images/{file_name.split('.')[0]}_DET.jpg', predictions)
 image = cv2.imread('visdrone_test_img_0000001_02999_d_0000005.jpg')
 predictions = coco_demo.run_on_opencv_image(image)
 #cv2.imwrite('drone_res.jpg', predictions)
